a58c0d2faf74a40376dd8604285bd9f94809e92a
Louie Lee <louielee.dev@gmail.com>
2024-03-11 15:04:40 +0800

    feat: prevent crawling and indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="robots" content="noindex" />
+        <title>My Private Page</title>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
