0c816beec352ee0341b4e92fe3eb0586be5b9729
Louie Lee <louielee.dev@gmail.com>
2024-03-14 12:04:18 +0800

    feat(backend): add get appointment api

--- src/app/appointment/route.ts
+++ src/app/appointment/route.ts
@@ -1,6 +1,15 @@
 import prisma from "@lib/prisma";
 
 export async function GET() {
-	const appointments = await prisma.appointment.findMany({});
+	const appointments = await prisma.appointment.findMany({
+		orderBy: [
+			{ startedAt: "asc" },
+			{ endedAt: "asc" },
+			{ createdAt: "asc" },
+			{ userId: "asc" },
+		],
+		include: { User: true },
+	});
+
 	return Response.json(appointments);
 }
