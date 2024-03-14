54c9cc51dac5b9c4d1c8f6cf66c92a635f0ed8ff
Louie Lee <louielee.dev@gmail.com>
2024-03-14 10:07:09 +0800

    feat: install tanstack query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,18 @@
 // app/providers.tsx
-'use client'
+"use client";
 
-import { NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
   return (
     <NextUIProvider>
-      {children}
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </NextUIProvider>
-  )
+  );
 }
