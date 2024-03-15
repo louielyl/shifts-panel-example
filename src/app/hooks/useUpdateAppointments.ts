bdd53662d09bf821372e053e1dcccf7391c8eaca
Louie Lee <louielee.dev@gmail.com>
2024-03-15 11:16:05 +0800

    feat(Shift): add useUpdateAppointments hook

---
+++ src/app/hooks/useUpdateAppointments.ts
@@ -0,0 +1,19 @@
+import { UseMutationOptions, useMutation } from "@tanstack/react-query";
+
+export type UpdateInfo = { id: string; status: string }[];
+
+export default function useUpdateAppointments(
+	options?: UseMutationOptions<any, Error, UpdateInfo, unknown>,
+) {
+	return useMutation({
+		mutationKey: ["shift-data"],
+		mutationFn: async (updateInfo: UpdateInfo) => {
+			const respond = await fetch("/appointment", {
+				method: "PATCH",
+				body: JSON.stringify(updateInfo),
+			});
+			return await respond.json();
+		},
+		...options,
+	});
+}
