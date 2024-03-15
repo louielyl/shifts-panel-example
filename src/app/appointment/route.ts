f9d7bce3c7085e0155bacc8994744744c7de37ef
Louie Lee <louielee.dev@gmail.com>
2024-03-15 11:15:41 +0800

    feat(backend): add patch API

--- src/app/appointment/route.ts
+++ src/app/appointment/route.ts
@@ -1,3 +1,4 @@
+import { Status } from "@/types";
 import prisma from "@lib/prisma";
 
 export async function GET() {
@@ -13,4 +14,20 @@ export async function GET() {
 	});
 
 	return Response.json(appointments);
+}
+
+export async function PATCH(request: Request) {
+	const data: { id: string; status: Status }[] = await request.json();
+	const response = await Promise.all(
+		data.map(async (datum) =>
+			prisma.appointment.update({
+				where: { id: datum.id },
+				data: {
+					status: datum.status,
+				},
+			}),
+		),
+	);
+
+	return Response.json(response);
 }
