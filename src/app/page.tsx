414748847e7a8c9670d16e22b3e89d9774a335bd
Louie Lee <louielee.dev@gmail.com>
2024-03-17 23:49:34 +0800

    feat(Shift): add pending filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Spacer, Spinner } from "@nextui-org/react";
+import { Checkbox, Spacer, Spinner } from "@nextui-org/react";
 import Notice from "./components/Notice";
 import SearchBar from "./components/SearchBar";
 import { useQuery } from "@tanstack/react-query";
@@ -11,6 +11,7 @@ import { useCallback, useState } from "react";
 
 export default function Home() {
   const [searchInput, setSearchInput] = useState("");
+  const [searchPendingOnly, setSearchPendingOnly] = useState(false);
 
   const searchInputContains = useCallback(
     (input: string | null) => {
@@ -20,7 +21,11 @@ export default function Home() {
     },
     [searchInput],
   );
-  const { data: dataByMonth, isSuccess, isLoading } = useQuery<
+  const {
+    data: dataByMonth,
+    isSuccess,
+    isLoading,
+  } = useQuery<
     (Appointment & { User: User })[],
     unknown,
     { [key: string]: (Appointment & { User: User })[] }
@@ -36,31 +41,43 @@ export default function Home() {
       }
     },
     select: (data) =>
-      data.reduce(
-        (acc: Record<string, (Appointment & { User: User })[]>, cur) => {
-          const month = dayjs(cur.startedAt).format("YYYY-MM");
-          acc[month] = acc[month] || [];
-          if (
-            !searchInput ||
-            searchInputContains(cur.User.chiName) ||
-            searchInputContains(cur.User.firstName) ||
-            searchInputContains(cur.User.lastName)
-          )
-            acc[month].push(cur);
-          return acc;
-        },
-        {},
-      ),
+      data
+        .filter((datum) =>
+          searchPendingOnly ? datum.status === "PENDING" : true,
+        )
+        .reduce(
+          (acc: Record<string, (Appointment & { User: User })[]>, cur) => {
+            const month = dayjs(cur.startedAt).format("YYYY-MM");
+            acc[month] = acc[month] || [];
+            if (
+              !searchInput ||
+              searchInputContains(cur.User.chiName) ||
+              searchInputContains(cur.User.firstName) ||
+              searchInputContains(cur.User.lastName)
+            )
+              acc[month].push(cur);
+            return acc;
+          },
+          {},
+        ),
   });
 
   return (
     <main className="m-6 flex flex-1 flex-col bg-white p-6 md:h-[calc(100vh_-_4rem)]">
       <Notice />
       <Spacer y={4} />
-      <SearchBar
-        value={searchInput}
-        onChange={(value) => setSearchInput(value)}
-      />
+      <div className="flex gap-2">
+        <SearchBar
+          value={searchInput}
+          onChange={(value) => setSearchInput(value)}
+        />
+        <Checkbox
+          isSelected={searchPendingOnly}
+          onClick={() => setSearchPendingOnly((cur) => !cur)}
+        >
+          <label>Pending only</label>
+        </Checkbox>
+      </div>
       <Spacer y={4} />
       {isLoading ? (
         <div className="grid h-full place-items-center">
