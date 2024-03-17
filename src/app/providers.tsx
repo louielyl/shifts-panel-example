05013bc5f14344a61772228e3c31d671b3af61a3
Louie Lee <louielee.dev@gmail.com>
2024-03-18 00:51:09 +0800

    feat(react-toastify): add package

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,8 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
@@ -12,6 +14,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <QueryClientProvider client={queryClient}>
         {children}
         <ReactQueryDevtools initialIsOpen={true} />
+        <ToastContainer />
       </QueryClientProvider>
     </NextUIProvider>
   );
